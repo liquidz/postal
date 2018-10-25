c6221230b10a4a5c1c5cc52ff805c9c03487dfed
liquidz <liquidz.uo@gmail.com>
2018-10-25 17:26:11 +0900

    Bump java-mail version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :comments "Use at your own risk"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[commons-codec "1.11"]
-                 [com.sun.mail/javax.mail "1.6.1"]
-                 [javax.mail/javax.mail-api "1.6.1"]]
+                 [com.sun.mail/javax.mail "1.6.2"]
+                 [javax.mail/javax.mail-api "1.6.2"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
