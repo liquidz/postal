84b5d12a6e120248b27d8c11c27863e9fa2e47fc
liquidz <liquidz.uo@gmail.com>
2019-03-28 14:09:50 +0900

    Bump JavaMail to 1.6.3

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
             :distribution :repo
             :comments "Use at your own risk"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[commons-codec "1.11"]
-                 [com.sun.mail/javax.mail "1.6.2"]
-                 [javax.mail/javax.mail-api "1.6.2"]]
+  :dependencies [[commons-codec "1.12"]
+                 [com.sun.mail/jakarta.mail "1.6.3"]
+                 [jakarta.mail/jakarta.mail-api "1.6.3"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-beta4"]]}}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}}
   :aliases
   {"test-all" ["with-profile" "1.9:1.10" "test"]})
